5f924ca363995c3eae526e55a6f59a0dbb3cf7e6
unknown <minh.huang96@gmail.com>
2019-08-13 12:29:51 -0700

    step 1-imported

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 
 //import any components needed
-
+import data from "../../../data";
 //Import your array data to from the provided data file
-
+import { specials } from '../../../data';
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [ specialsState, setSpecialsState] = useState( specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
